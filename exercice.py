986bc6533e7b5e4588f3d0d8d8ab5db91cf05e83
SilentPow <gabcoteprive@gmail.com>
2020-09-14 17:32:45 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    valeurExacte = angle_degs + angle_mins*
+    valeurExacte = angle_degs + angle_mins * 60 + 
     
     return 0.0
 
