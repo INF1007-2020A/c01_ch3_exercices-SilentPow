d46c3472d0b30456ad266703d8860f331582877e
SilentPow <gabcoteprive@gmail.com>
2020-09-14 17:45:34 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -26,6 +26,7 @@ def to_degrees(angle_rads: float) -> tuple:
     valeurExacte = angle_rads * 180 / math.pi
     angle_degrees = math.trunc(valeurExacte)
     minutes = math.trunc((valeurExacte - angle_degrees)*60)
+    seconds = 
 
     return 0.0, 0.0, 0.0
 
