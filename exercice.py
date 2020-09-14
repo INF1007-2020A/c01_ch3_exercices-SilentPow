a588527328d582cbf7b2463c0881aac48ba57f42
SilentPow <gabcoteprive@gmail.com>
2020-09-14 17:12:04 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,8 +9,8 @@ def average(a: float, b: float, c: float) -> float:
     moyenne = 0
     valeurs = [a,b,c]
     
-    moyenne = valeurs[i] for i in valeurs
-    
+    moyenne += valeurs[i] for i in valeurs
+    moyenne
 
     return 0.0
 
