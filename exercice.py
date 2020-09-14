28ea69d2ef0b0de9ef348efd9ac755d0340cd770
SilentPow <gabcoteprive@gmail.com>
2020-09-14 17:43:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,7 +24,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     valeurExacte = angle_rads * 180 / math.pi
-    math.trunc(valeurExacte)
+    angle_degrees = math.trunc(valeurExacte)
 
     return 0.0, 0.0, 0.0
 
